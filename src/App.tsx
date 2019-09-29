fbeed82b4e538b9a5d973194cee591055ac44c83
vytautassugintas <vytautas.sugintas@gmail.com>
2019-09-29 20:36:08 +0300

    feat: social info

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import styles from "./App.module.css";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.page}>
+      <div className={styles.container}>
+        <h1>Vytautas Sugintas</h1>
+        <p className="text-sm">Front End Engineer at 1stdibs</p>
+        <div className={styles.infoContainer}>
+          <p className={styles.infoLine}>
+            <i className="icon" data-feather="map-pin"/>
+            Lithuania, Vilnius
+          </p>
+          <p className={styles.infoLine}>
+            <i className="icon" data-feather="mail"/>
+            <a href="mailto:vytautas.sugintas@gmail.com">vytautas.sugintas@gmail.com</a>
+          </p>
+          <p className={styles.infoLine}>
+            <i className="icon" data-feather="github"/>
+            <a href="https://github.com/vytautassugintas">vytautassugintas</a>
+          </p>
+          <p className={styles.infoLine}>
+            <i className="icon" data-feather="twitter"/>
+            <a href="https://twitter.com/vytautasugintas">vytautasugintas</a>
+          </p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import feather from 'feather-icons';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+// replaces all feather icons into svgs
+feather.replace()
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
